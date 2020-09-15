ea9daae5e26c3184ff5cb1476fc692ec2fabc0e7
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-15 00:24:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,11 +11,11 @@ def using_pop(array)
 end
 
 def pop_with_args(array)
-  array.pop[-2]
+  array.pop(-2)
 end
 
 def using_shift(array)
-  array.shift[0]
+  array.shift()
 end
 
 def shift_with_args(array)
